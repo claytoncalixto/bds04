2e9d0dd02865b6dcdd16e7732b47dcf6d914dfba
claytoncalixto <claytonrcalixto@gmail.com>
2021-07-21 23:48:23 -0300

    03-33 Challenge to deliver - Part 1

--- src/main/java/com/devsuperior/bds04/dto/EventDTO.java
+++ src/main/java/com/devsuperior/bds04/dto/EventDTO.java
@@ -3,15 +3,25 @@ package com.devsuperior.bds04.dto;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import com.devsuperior.bds04.entities.Event;
 
 public class EventDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	private Long id;
+	
+	@NotBlank(message = "Nome não pode ser vazio")
 	private String name;
+	
+    @FutureOrPresent(message = "A data não pode ser no passado")
 	private LocalDate date;
 	private String url;
+	
+	@NotNull(message = "Cidade não pode ser nula")
 	private Long cityId;
 	
 	public EventDTO() {
--- src/test/java/com/devsuperior/bds04/controllers/CityControllerIT.java
+++ src/test/java/com/devsuperior/bds04/controllers/CityControllerIT.java
@@ -2,6 +2,7 @@ package com.devsuperior.bds04.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
